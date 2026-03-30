3de476977ce07b6efb36e5d1ad46ef0278499cba
Nithi <nitheshwaran003@gmail.com>
2026-03-30 13:13:28 +0530

    btn component

---
+++ src/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+
+const Button = () => {
+  return <button className="brand-button">Sample Button</button>;
+};
+
+export default Button;
--- src/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import '../../src/App.css'
-
-function Button() {
-  return (
-    <button className="brand-button">Hello Hey</button>
-  )
-}
-
-export default Button
