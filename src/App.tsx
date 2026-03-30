1abc15acc596865344d369895efa67f016986bf4
Nithi <nitheshwaran003@gmail.com>
2026-03-30 12:52:13 +0530

    commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="hello">
+      Main sample text
+    </div>
+  )
+}
+
+export default App
---
+++ src/Button/index.tsx
@@ -0,0 +1,9 @@
+import '../../src/App.css'
+
+function Button() {
+  return (
+    <button className="brand-button">Hello Hey</button>
+  )
+}
+
+export default Button
